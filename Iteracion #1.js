61fa874d9e6fdf2180e5b55c5d037e8dbcd04735
Jordi Peña Bootcamp <116197945+JordiAS1981@users.noreply.github.com>
2022-12-05 21:10:37 +0100

    cambio

---
+++ Iteracion #1.js
@@ -0,0 +1,10 @@
+function sum(numberOne, numberTwo) {
+    let masAlto;
+    if (numberOne >= numberTwo) {
+        masAlto = numberOne;
+    }
+    else {
+        masAlto = numberTwo;
+    }
+    return masAlto;
+}
