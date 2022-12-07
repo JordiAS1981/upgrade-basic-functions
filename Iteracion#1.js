ff3045bf36110790c8b0202c9ede405beca8deb3
Jordi Peña Bootcamp <116197945+JordiAS1981@users.noreply.github.com>
2022-12-07 12:34:03 +0100

    acabado

---
+++ Iteracion#1.js
@@ -0,0 +1,11 @@
+function sum(numberOne, numberTwo) {
+    if (numberOne>numberTwo) {
+        return numberOne;
+    }
+    else {
+        return numberTwo;
+    }
+}
+
+let masAlto = sum(1,9);
+console.log(masAlto)
