436dae7821d6e0851759e484a75f44522a3fd1f7
Jordi Peña Bootcamp <116197945+JordiAS1981@users.noreply.github.com>
2022-12-27 21:21:04 +0100

    Iteracion#2

--- Iteracion#2.js
+++ Iteracion#2.js
@@ -1,4 +1,11 @@
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-function findLongestWord(param) {
-    
+function findLongestWord(avengers) {
+    let longestWord = avengers[0];
+    for (i=0; i<avengers.length; i++) {
+        if (avengers[i].length > longestWord.length) {
+            longestWord = avengers[i];
+        }
+    }
+    return longestWord;
 }
+console.log(findLongestWord(avengers));
